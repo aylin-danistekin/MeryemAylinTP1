c3f1d200db93b387387c6a9f2614820aa0fcf935
Meryem <berramoumeryem@gmail.com>
2026-09-23 15:19:24 +0200

    page tp1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,25 +1,16 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 import { Header } from './header/header';
 import { Footer } from './footer/footer';
 
 @Component({
-  imports: [RouterOutlet, Header, Footer],
+  imports: [Header, Footer],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
-  public mesPrenoms: string = "Meryem et Aylin";
-  public textOutput: string = "";
+  public couleurFond: string = 'white';
 
-  public headerOutput(headerText: string): void {
-    this.textOutput = headerText;
-  }
-
-  public footerOutput(footerText: string): void {
-    this.textOutput = footerText;
-  }
   public changerCouleur(couleur: string): void {
     this.couleurFond = couleur;
   }
